1e3149510216c1b0ab65f5c18d1a12fc2d720b1b
Nazat Adi <149302691+Nazathossain@users.noreply.github.com>
2024-09-12 09:55:09 +0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      external: ['/src/main.jsx'], // Externalize if needed
+    },
+  },
+});
